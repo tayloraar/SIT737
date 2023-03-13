1321e1e5c9951861b8bb2b3f6f659e39e3ecb2d1
Aaron <2aaronjt@gmail.com>
2023-03-13 17:21:03 +1100

    Final Commit Added Date and Time

--- Week1/server.js
+++ Week1/server.js
@@ -2,6 +2,13 @@ var express = require(`express`);
 var app = express();
 
 
+app.use((req,res,next)=>{
+     
+   
+    console.log(`A new request received at ${Date(Date.now())}`);
+    next();
+})
+
 app.get(`/`, (req, res)=> res.send(`Hello World`));
 app.get(`/cheese`, (req, res) => res.send(`You hit Cheese`));
 
